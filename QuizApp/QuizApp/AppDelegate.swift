6d5be5728dfda8073aa37bcfc30a7e62cd99f89b
Denis Pavlov <denis.pavlov@simbirsoft.com>
2019-09-29 17:31:33 +0400

    project initialization

---
+++ QuizApp/QuizApp/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  QuizApp
+//
+//  Created by Денис on 29.09.2019.
+//  Copyright © 2019 Kurumoch Team LLC. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ QuizApp/QuizApp/Classes/Presentations/MainMenu/MainMenuViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MainMenuViewController.swift
+//  QuizApp
+//
+//  Created by Денис on 29.09.2019.
+//  Copyright © 2019 Kurumoch Team LLC. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {}
