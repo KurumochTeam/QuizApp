c210ec5a6f02bff3a1a7764c6f40d9778a00bef1
Aleksandr Sidorov <sanik3d@gmail.com>
2019-10-13 21:06:28 +0400

    task-1-fix Fix MenuButton's shadow layer

--- QuizApp/QuizApp/Classes/Common Views/MenuButton/MenuButton.swift
+++ QuizApp/QuizApp/Classes/Common Views/MenuButton/MenuButton.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 final class MenuButton: UIView, NibOwnerLoadable {
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet private weak var title: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var containerView: UIView!
     
     var delegate: ButtonDelegate?
     
@@ -22,6 +23,12 @@ final class MenuButton: UIView, NibOwnerLoadable {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         loadNibContent()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        addCornerRadius()
+        addFloatingShadow()
     }
     
     func configure(with title: String, and image: Image) {
@@ -44,4 +51,39 @@ extension MenuButton {
 
 protocol ButtonDelegate {
     func didButtonTap(_ button: MenuButton)
+}
+
+private extension MenuButton {
+    struct LayerConstants {
+        static let relativeWidth = CGFloat(0.83)
+        static let shadowRadius = CGFloat(40)
+    }
+    
+    func addCornerRadius() {
+        containerView.layer.cornerRadius = layer.frame.height * LayerConstants.relativeWidth / 2
+        containerView.layer.masksToBounds = true
+    }
+    
+    func addFloatingShadow() {
+        let shadowLayerWidth = layer.frame.width - containerView.layer.cornerRadius / 2
+        let shadowLayer = CALayer()
+        shadowLayer.frame = CGRect(
+            x: (layer.frame.width - shadowLayerWidth) / 2,
+            y: layer.frame.height / 2,
+            width: shadowLayerWidth,
+            height: layer.frame.height / 2
+        )
+        
+        shadowLayer.cornerRadius = containerView.layer.cornerRadius
+        
+        shadowLayer.shadowOpacity = 1.0
+        shadowLayer.backgroundColor = UIColor.red.cgColor
+        shadowLayer.shadowColor = UIColor.violetMain.cgColor
+        shadowLayer.shadowOffset = CGSize(width: 0, height: 8)
+        shadowLayer.shadowRadius = LayerConstants.shadowRadius
+        shadowLayer.masksToBounds = false
+        layer.masksToBounds = false
+        
+        layer.insertSublayer(shadowLayer, at: 0)
+    }
 }
