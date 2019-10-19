781578795d80271b3a6102b990a26e9dbbc2e3cf
Aleksandr Sidorov <sanik3d@gmail.com>
2019-10-19 23:21:17 +0400

    [task-1] Add background and title

--- QuizApp/QuizApp/Classes/Presentation/MainMenu/MainMenuViewController.swift
+++ QuizApp/QuizApp/Classes/Presentation/MainMenu/MainMenuViewController.swift
@@ -12,12 +12,22 @@ final class MainMenuViewController: UIViewController {
     @IBOutlet private var playMenuButtom: MenuButton!
     @IBOutlet private var rankingsMenuButton: MenuButton!
     @IBOutlet private var settingsMenuButton: MenuButton!
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var buttonsStackView: UIStackView!
+    
+    private var backgroundLayer: CALayer!
     
     var presenter: MainMenuPresenter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureButtons()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        configureBackground()
+        configureTitle()
     }
     
     private func configureButtons() {
@@ -37,6 +47,48 @@ final class MainMenuViewController: UIViewController {
             title: R.string.localizable.settings(),
             image: UIImage(systemName: "gear"),
             tapAction: presenter?.onSettingsPressed
+        )
+    }
+}
+
+private extension MainMenuViewController {
+    struct LayerConstants {
+        static let backgroundRotation = 0.42 * CGFloat.pi
+        static let additionalOffset = CGFloat(-40)
+    }
+    
+    func configureBackground() {
+        if backgroundLayer == nil {
+            backgroundLayer = CALayer()
+            backgroundLayer.backgroundColor = R.color.violetSecondary()?.cgColor
+            view.layer.insertSublayer(backgroundLayer, at: 0)
+        }
+        
+        let backgroundYOffset = buttonsStackView.center.y - view.center.y
+        
+        backgroundLayer.frame = CGRect(
+            x: 0,
+            y: backgroundYOffset + LayerConstants.additionalOffset,
+            width: buttonsStackView.frame.height + backgroundYOffset,
+            height: view.frame.height
+        )
+        
+        backgroundLayer.setAffineTransform(
+            CGAffineTransform(rotationAngle: LayerConstants.backgroundRotation)
+        )
+    }
+    
+    func configureTitle() {
+        titleLabel.frame = CGRect(
+            x: buttonsStackView.frame.origin.x,
+            y: buttonsStackView.frame.origin.y
+                - titleLabel.frame.height + LayerConstants.additionalOffset,
+            width: titleLabel.frame.width,
+            height: titleLabel.frame.height
+        )
+        
+        titleLabel.layer.setAffineTransform(
+            CGAffineTransform(rotationAngle: LayerConstants.backgroundRotation - CGFloat.pi / 2)
         )
     }
 }
